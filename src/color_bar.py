a59e5d44bde7281bfb8e0f832a48831b73d0dfb1
Patrick Crumley <patrick.crumley@gmail.com>
2018-03-06 00:28:44 -0500

    added colobrar

--- src/color_bar.py
+++ src/color_bar.py
@@ -4,7 +4,7 @@ import numpy as np
 def make_color_bar(cmap='viridis', cnorm = 'log',
                        pow_zero = '0', pow_gamma='1.0',
                        vmin = '0', vmax = '1',
-                       clip = 'true', interpolation = 'bicubic',
+                       clip = 'false', interpolation = 'bicubic',
                        px ='30', py='400', alignment='vertical'):
     '''First we calculate the histogram, then we turn it into an image and return
     the image as a bytesIO'''
@@ -20,7 +20,7 @@ def make_color_bar(cmap='viridis', cnorm = 'log',
     if cnorm =='pow':
         color_bar.setNorm('pow',zero = float(pow_zero), gamma = float(pow_gamma),clipped = True if clip =='true' else False)
     else:
-        color_bar.setNorm('linear',clipped = True if clip =='true' else False)
+        color_bar.setNorm('linear', clipped = True if clip =='true' else False)
     color_bar.setCmap(cmap)
     color_bar.set_clim(cmin = float(vmin), cmax = float(vmax))
     color_bar.set_aspect(0)

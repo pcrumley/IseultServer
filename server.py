b3079352bd7220db579d5eb3104508853763b55b
Patrick Crumley <patrick.crumley@gmail.com>
2018-03-22 17:19:26 -0400

    server now returns available sim_types. We need a better way of doing this.

--- server.py
+++ server.py
@@ -48,36 +48,13 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_t
         return update_wrapper(wrapped_function, f)
     return decorator
 
-"""
-@app.route('/api/field/imgs/')
-def field_image():
-
-    query_dict = {}
-    arg = request.args.get('n')
-    if arg:
-        print(arg)
-        fpath = 'test_output/flds.tot.'+arg.zfill(3)
-        with h5py.File(fpath, 'r') as f:
-            Bz1 = np.array(f['bz'][0,:,:],dtype = 'f8')
-
-        im1 = myFieldImage(400, 400)
-        im1.setData(Bz1)
-        im1.setExtent([0,Bz1.shape[1], 0, Bz1.shape[0]])
-        im1.setNorm('pow', zero = 0.0, gamma = 1.0)
-        im1.setCmap('Blue-Black-Red')
-        im1.renderImage()
-        img_io = io.BytesIO()
-        im1.img.save(img_io, format='png',compress_level = 1)#, quality=100)
-        img_io.seek(0)
-        return send_file(img_io, mimetype='image/png')
-        img_io.close()
-    abort(404)
-"""
 
 @app.route('/api/handshake')
 @crossdomain(origin='*')
 def handshake():
-    return jsonify({'name':'IseultServer', 'version': 'alpha'})
+    return jsonify({'name':'IseultServer',
+                    'version': 'alpha',
+                    'sim_types': ['tristan-mp']})
 
 @app.route('/api/2dhist/imgs/')
 @crossdomain(origin='*')

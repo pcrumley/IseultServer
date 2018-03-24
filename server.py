077b91f7d40f9473edfdff2016fb6cf07883f410
Patrick Crumley <patrick.crumley@gmail.com>
2018-03-23 22:34:07 -0400

    added an api to traverse the file directory of the server

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_file, request, abort, jsonify, make_response, current_app
-import sys
+import sys, os
 sys.path.insert(0, './src/')
 from particle_hist import make_2d_hist_img
 from color_bar import make_color_bar
@@ -54,7 +54,8 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_t
 def handshake():
     return jsonify({'name':'IseultServer',
                     'version': 'alpha',
-                    'sim_types': ['tristan-mp']})
+                    'sim_types': ['tristan-mp'],
+                    'server_dir': os.path.split(os.path.abspath(os.curdir))[0]})
 
 @app.route('/api/2dhist/imgs/')
 @crossdomain(origin='*')
@@ -73,6 +74,34 @@ def hist2d_image():
 
     #return jsonify(query_dict)
     abort(404)
+
+@app.route('/dirs/', defaults={'req_path': ''})
+@app.route('/dirs/<path:req_path>')
+@crossdomain(origin='*')
+def dir_listing(req_path):
+    BASE_DIR = '/'
+
+    # Joining the base and the requested path
+    abs_path = os.path.abspath(os.path.join(BASE_DIR, req_path))
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return abort(404)
+
+    dirList = []
+    fileList = []
+    with os.scandir(abs_path) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and not entry.name.startswith('_'):
+                if entry.is_dir():
+                    dirList.append(entry.name)
+                elif entry.is_file():
+                    fileList.append(entry.name)
+    return jsonify({'parentDir': os.path.split(abs_path)[0], 'dirs': dirList, 'files': fileList})
 
 @app.route('/api/colorbar/')
 @crossdomain(origin='*')
